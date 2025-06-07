9a472614b94336326a0378c350b73c3725f8fc14
dillu0044 <delhiraokola@gmail.com>
2025-06-07 11:43:07 +0530

    update

---
+++ main.js
@@ -0,0 +1,25 @@
+const display =document.getElementById('display');
+function calculate(input){
+  display.value += input;
+  if (display.value === '1+'){
+    display.value = 'NEVER SETTLE';
+    display.style.color ='red';
+  }
+  
+}
+function clearScreen(){
+  display.value="";
+}
+function calculater() {
+  try {
+    // Tab to edit
+    display.value = eval(display.value);
+  } catch (error) {
+     display.value ='error';
+     display.style.color='red';
+  }
+
+}
+function backspace(){
+  display.value = display.value.slice(0,-1);
+}
